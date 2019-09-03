77b0f6cb63b7678b6b1938e6fa7aa0bdda7bd2d3
Kaushik Sarma <kaushik.sarma@ET-C02Y86JKJG5H.local>
2019-09-03 12:04:24 +0530

    Responsive css added

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -24,20 +24,23 @@ export default class ProductItem extends React.Component<ProductItemProps> {
     render = () => {
         const hasDiscount = this.props.price.mrp !== undefined && !isNaN(this.props.price.mrp);
         const discountPercent = hasDiscount ? this.discount(this.props.price.mrp, this.props.price.final_price) : 0;
-        return (<div className='product-item'>
+        return (
+        <div className='product-item'>
             <div className='image-container'>
             <img src={this.props.image} alt="Could not display image!"></img>
             </div>
-            <p className='product-title'>{this.props.title}</p>
-            <div className='rating-row'>
-                <span className="color-display-bubble" style={{background: this.props.colour.color}}></span><label>{this.props.colour.title}</label>
-                { this.props.rating !== undefined && this.props.rating !== 0 && <span className='rating-badge'>{this.props.rating} &#9734;</span> }
+            <div className='product-desc'>
+                <p className='product-title'>{this.props.title}</p>
+                <div className='rating-row'>
+                    <span className="color-display-bubble" style={{background: this.props.colour.color}}></span><label>{this.props.colour.title}</label>
+                    { this.props.rating !== undefined && this.props.rating !== 0 && <span className='rating-badge'>{this.props.rating} &#9734;</span> }
+                </div>
+                <p>
+                    <span className='price-final'>&#8377;{this.props.price.final_price}</span>
+                { hasDiscount && <span className='price-mrp'>&#8377;{this.props.price.mrp}</span> }
+                { hasDiscount && <span className='price-discount'>{discountPercent}&#37; off</span>}
+                </p>
             </div>
-            <p>
-                <span className='price-final'>&#8377;{this.props.price.final_price}</span>
-            { hasDiscount && <span className='price-mrp'>&#8377;{this.props.price.mrp}</span> }
-            { hasDiscount && <span className='price-discount'>{discountPercent}&#37; off</span>}
-            </p>
         </div>)
     }
 }
--- src/components/SearchFiltersPane/SearchFiltersPane.tsx
+++ src/components/SearchFiltersPane/SearchFiltersPane.tsx
@@ -1,129 +1,235 @@
-import * as React from 'react';
+import * as React from "react";
 
-import './index.scss';
+import "./index.scss";
 
 interface SearchFilterProps {
-    filters: {
-        BRAND:{type:string, values:{}[] } ,
-        COLOR: {type:string, values:{}[] },
-        PRICE: {type:string, values:{}[] },
-    }
-    setFilters(filters);
+  filters: {
+    BRAND: { type: string; values: {}[] };
+    COLOR: { type: string; values: {}[] };
+    PRICE: { type: string; values: {}[] };
+  };
+  setFilters(filters);
 }
 
 interface SearchFilterState {
-    minprice: string;
-    maxprice: string;
-    brand ?: string;
-    colors ?: boolean[];
+  minprice: string;
+  maxprice: string;
+  brand?: string;
+  colors?: boolean[];
 }
-export default class SearchFilterPane extends React.Component<SearchFilterProps, SearchFilterState> {
-    constructor (props) {
-        super(props);
-        this.state = {
-            minprice: "Min",
-            maxprice: "Max",
-            brand: "",
-            colors: new Array(this.getNumColors()).fill(true)
-        };
+
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
     }
 
-    exists (object) {
-        return object !== null  && object !== undefined && object !== {} && object.length > 0;
-    }
-    getNumColors = ():number => {
-        if (this.exists(this.props.filters.COLOR) && this.exists(this.props.filters.COLOR.values)) {
-            return this.props.filters.COLOR.values.length;
-        }
-        return 0;
-    }
-    getSelectedColors = (): {} =>
-    this.props.filters.COLOR.values.filter((c, index) => this.state.colors[index])
-    
-    
-    updateFilters = event => {
-        const name = event.target.name;
-        const val = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-        switch(name) {
-            case 'minprice':
-            {   event.preventDefault();
-                const minindex = this.props.filters.PRICE.values.findIndex(e => e['key'] === val);
-                const maxindex = this.props.filters.PRICE.values.findIndex(e => e['key'] === this.state.maxprice);
-                minindex < maxindex && this.setState({ minprice: val }, () => {
-                    this.getSearchReults();
-                });
-                break;
-            }
-            case 'maxprice': 
-            {   event.preventDefault();
-                const minindex = this.props.filters.PRICE.values.findIndex(e => e['key'] === this.state.minprice);
-                const maxindex = this.props.filters.PRICE.values.findIndex(e => e['key'] === val);
-                minindex < maxindex && this.setState({ maxprice: val }, () => {
-                    this.getSearchReults();
-                });
-                break;
-            }
-            case 'brand': 
-            {   event.preventDefault();
-                this.setState({
-                    brand: val
-                }, () => {
-                    this.getSearchReults();
-                });
-                break;
-            }
-            case 'colorChoices':
-            {   
-                let newstate = this.state.colors;
-                newstate[event.target.dataset['index']] = val;
-                this.setState({
-                    colors: newstate
-                }, () => {
-                    this.getSearchReults();
-                });                
-                break;
-            }
-        }
-    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    getSearchReults = () => {
-        this.props.setFilters({
-            minprice: this.state.minprice,
-            maxprice: this.state.maxprice,
-            brand: this.state.brand,
-            colors: this.getSelectedColors()
-        });
-    }
+  return windowDimensions;
+}
 
-    render = () => {
-        return (
-            <form className="search-filters position-absolute">
-                <div className="search-filter-group">
-                    <h3>Filters</h3>
-                </div>
-                <div className="search-filter-group">
-                    <h4>Price</h4>
-                    <select name="minprice" value={this.state.minprice} onChange={this.updateFilters}>
-                        {this.props.filters.PRICE.values !== undefined && this.props.filters.PRICE.values.map(value => <option key={`min-${value['key']}`} value={value['key']}>{value['displayValue']}</option>)}
-                    </select>
-                    <i className="separator">to</i>
-                    <select name="maxprice" value={this.state.maxprice} onChange={this.updateFilters}>
-                        {this.props.filters.PRICE.values !== undefined && this.props.filters.PRICE.values.map(value => <option key={`max-${value['key']}`} value={value['key']}>{value['displayValue']}</option>)}
-                    </select>
-                </div>   
-                <div className="search-filter-group">
-                    <h4>Brand</h4>
-                    <input type="text" name="brand" placeholder="Search Brand" onChange={this.updateFilters} value={this.state.brand}></input>
-                </div>
-                <div className="search-filter-group">
-                    <h4>Color</h4>
-                    {this.exists(this.props.filters.COLOR.values)
-                     && this.props.filters.COLOR.values.map((choice, index) =>  {
-                        return <div key={`color-${choice['title']}`} className='color-input'><input type="checkbox" name="colorChoices" onChange={this.updateFilters} data-index={index} checked={this.state.colors[index]}></input><span className="color-display-bubble" style={{background: choice['color']}}></span><label>{choice['title']}</label></div>
-                    })}
-                    <br/>
-                </div>
-            </form>
+export default class SearchFilterPane extends React.Component<
+  SearchFilterProps,
+  SearchFilterState
+> {
+  height: number;
+  width: number;
+
+  constructor(props) {
+    super(props);
+    // const { height, width } = useWindowDimensions();
+
+    // this.height = height;
+    // this.width = width;
+
+    this.state = {
+      minprice: "Min",
+      maxprice: "Max",
+      brand: "",
+      colors: new Array(this.getNumColors()).fill(true)
+    };
+  }
+
+  exists(object) {
+    return (
+      object !== null &&
+      object !== undefined &&
+      object !== {} &&
+      object.length > 0
+    );
+  }
+  getNumColors = (): number => {
+    if (
+      this.exists(this.props.filters.COLOR) &&
+      this.exists(this.props.filters.COLOR.values)
+    ) {
+      return this.props.filters.COLOR.values.length;
+    }
+    return 0;
+  };
+  getSelectedColors = (): {} =>
+    this.props.filters.COLOR.values.filter(
+      (c, index) => this.state.colors[index]
+    );
+
+  updateFilters = event => {
+    const name = event.target.name;
+    const val =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    switch (name) {
+      case "minprice": {
+        event.preventDefault();
+        const minindex = this.props.filters.PRICE.values.findIndex(
+          e => e["key"] === val
         );
+        const maxindex = this.props.filters.PRICE.values.findIndex(
+          e => e["key"] === this.state.maxprice
+        );
+        minindex < maxindex &&
+          this.setState({ minprice: val }, () => {
+            this.getSearchReults();
+          });
+        break;
+      }
+      case "maxprice": {
+        event.preventDefault();
+        const minindex = this.props.filters.PRICE.values.findIndex(
+          e => e["key"] === this.state.minprice
+        );
+        const maxindex = this.props.filters.PRICE.values.findIndex(
+          e => e["key"] === val
+        );
+        minindex < maxindex &&
+          this.setState({ maxprice: val }, () => {
+            this.getSearchReults();
+          });
+        break;
+      }
+      case "brand": {
+        event.preventDefault();
+        this.setState(
+          {
+            brand: val
+          },
+          () => {
+            this.getSearchReults();
+          }
+        );
+        break;
+      }
+      case "colorChoices": {
+        let newstate = this.state.colors;
+        newstate[event.target.dataset["index"]] = val;
+        this.setState(
+          {
+            colors: newstate
+          },
+          () => {
+            this.getSearchReults();
+          }
+        );
+        break;
+      }
     }
+  };
+
+  getSearchReults = () => {
+    this.props.setFilters({
+      minprice: this.state.minprice,
+      maxprice: this.state.maxprice,
+      brand: this.state.brand,
+      colors: this.getSelectedColors()
+    });
+  };
+
+  render = () => {
+      console.log(window.innerWidth);
+    return (
+      <form className="search-filters position-absolute">
+        <div className="search-filter-group">
+          <h3>Filters</h3>
+        </div>
+        <div className="search-filter-group">
+          <h4>Price</h4>
+          <select
+            name="minprice"
+            value={this.state.minprice}
+            onChange={this.updateFilters}
+          >
+            {this.props.filters.PRICE.values !== undefined &&
+              this.props.filters.PRICE.values.map(value => (
+                <option key={`min-${value["key"]}`} value={value["key"]}>
+                  {value["displayValue"]}
+                </option>
+              ))}
+          </select>
+          <i className="separator">to</i>
+          <select
+            name="maxprice"
+            value={this.state.maxprice}
+            onChange={this.updateFilters}
+          >
+            {this.props.filters.PRICE.values !== undefined &&
+              this.props.filters.PRICE.values.map(value => (
+                <option key={`max-${value["key"]}`} value={value["key"]}>
+                  {value["displayValue"]}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="search-filter-group">
+          <h4>Brand</h4>
+          <input
+            type="text"
+            name="brand"
+            placeholder="Search Brand"
+            onChange={this.updateFilters}
+            value={this.state.brand}
+          ></input>
+        </div>
+        <div className="search-filter-group">
+          <h4>Color</h4>
+          {this.exists(this.props.filters.COLOR.values) &&
+            this.props.filters.COLOR.values.map((choice, index) => {
+              return (
+                <div key={`color-${choice["title"]}`} className="color-input">
+                  <input
+                    type="checkbox"
+                    name="colorChoices"
+                    onChange={this.updateFilters}
+                    data-index={index}
+                    checked={this.state.colors[index]}
+                  ></input>
+                  <span
+                    className="color-display-bubble"
+                    style={{ background: choice["color"] }}
+                  ></span>
+                  <label>{choice["title"]}</label>
+                </div>
+              );
+            })}
+          <br />
+        </div>
+      </form>
+    );
+  };
 }
